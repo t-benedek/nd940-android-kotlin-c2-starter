3a692eab6970a52208f0a7f4b5edea83a3989b70
Benedek Thomas <q436491@lmucm977588.fritz.box>
2023-03-05 17:00:18 +0100

    Added Background Worker

--- starter/app/src/main/java/com/udacity/asteroidradar/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/MainActivity.kt
@@ -1,15 +1,46 @@
 package com.udacity.asteroidradar
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import timber.log.Timber
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.*
+import com.udacity.asteroidradar.work.RefreshDataWork
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
+
+
+    val applicationScope = CoroutineScope(Dispatchers.Default)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        delayedInit()
+    }
+
+    private fun delayedInit(){
+        applicationScope.launch {
+            setupRecurringWork()
+        }
+    }
+    private fun setupRecurringWork(){
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.UNMETERED)
+            .setRequiresBatteryNotLow(true)
+            .setRequiresCharging(true)
+            .apply {
+                setRequiresDeviceIdle(true)
+            }.build()
+        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWork>(1, TimeUnit.DAYS)
+            .setConstraints(constraints)
+            .build()
+        WorkManager.getInstance().enqueueUniquePeriodicWork(
+            RefreshDataWork.WORK_NAME,
+            ExistingPeriodicWorkPolicy.KEEP,
+            repeatingRequest)
     }
 }
